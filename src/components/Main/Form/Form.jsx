3bfd41b8e40980d25fb1f7ad693d951b10f20ba1
Peroy Nel <peroynel@yahoo.com>
2022-01-06 12:05:50 +0200

    formatDate implemented
    formatDate implemented so that the date is in a useable format for Material-UI

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -10,11 +10,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
 
+import formatDate from '../../../utils/formatDate';
+
 const initialState = {
     amount: '',
     category: '',
     type: 'Income',
-    date: new Date(),
+    date: formatDate(new Date()),
 
 }
 
@@ -69,7 +71,7 @@ const Form = () => {
                 <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} />
             </Grid>
             <Grid item xs={6}>
-                <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: e.target.value })} />
+                <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })} />
             </Grid>
             <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}>Create
 
